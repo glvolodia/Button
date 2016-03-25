fffde32365d96435784c70f28aaebdfebc8c689d
Volodia Gl <Volodia Gl>
2016-03-25 16:18:47 +0200

    change index and script failes

--- js/script.js
+++ js/script.js
@@ -1,36 +1,57 @@
-    var data = {
-      "Рыбы": {
-        "Форель": {},
-        "Щука": {}
+ var data = [
+      {
+        name: 'Menu Header 1',
+        subitems: [
+          {
+            name: 'Menu subitem 1-1',
+            subitems: []
+          },
+          {
+            name: 'Menu subitem 1-2',
+            subitems:[
+              {
+                name: 'Menu subitem 1-2-1',
+                subitems: []
+              }
+            ]
+          }
+        ]
       },
+      {
+        name: 'Menu Header 2',
+        subitems: [
+          {
+            name: 'Menu subitem 2-1',
+            subitems: []
+          }
+        ]
+      },
+      {
+        name: 'Menu Header 2',
+        subitems: []  
+      }
+    ];
 
-      "Деревья": {
-        "Хвойные": {
-          "Лиственница": {},
-          "Ель": {}
-        },
-        "Цветковые": {
-          "Берёза": {},
-          "Тополь": {}
+      function createTree(container, obj) {
+        container.innerHTML = createTreeText(obj);
+      }
+      
+      function createTreeText(obj) {
+        var li = '';
+        
+        for(var i = 0; i < obj.length; i++){
+          if(obj[i].subitems){
+            li += '<li>' + obj[i].name + createTreeText(obj[i].subitems) + '</li>';
+          } else {
+            li += '<li>' + obj[i].name + '</li>';
+          }
+        }  
+           
+        if (li) {
+          var ul = '<ul>' + li + '</ul>'
         }
-
+        return ul || '';
       }
-    };
-
-    function createTree(container, obj) {
-      container.innerHTML = createTreeText(obj);
-    }
-
-    function createTreeText(obj) { // отдельная рекурсивная функция
-      var li = '';
-      for (var key in obj) {
-        li += '<li>' + key + createTreeText(obj[key]) + '</li>';
-      }
-      if (li) {
-        var ul = '<ul>' + li + '</ul>'
-      }
-      return ul || '';
-    }
-
-    var container = document.getElementById('container');
-    
+      
+      var container = document.getElementById('container');
+      
